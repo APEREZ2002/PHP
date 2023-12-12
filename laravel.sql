3c46f838c77e7a3b73fd034f104a3890ee306e96
APEREZ2002 <119208468+APEREZ2002@users.noreply.github.com>
2023-12-12 20:22:15 +0100

    Add files via upload

--- laravel.sql
+++ laravel.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-12-2023 a las 13:24:44
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 12-12-2023 a las 20:18:10
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,11 +29,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `failed_jobs` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `uuid` varchar(255) NOT NULL,
-  `connection` text NOT NULL,
-  `queue` text NOT NULL,
-  `payload` longtext NOT NULL,
-  `exception` longtext NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -45,7 +45,7 @@ CREATE TABLE `failed_jobs` (
 
 CREATE TABLE `migrations` (
   `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -68,8 +68,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 --
 
 CREATE TABLE `password_reset_tokens` (
-  `email` varchar(255) NOT NULL,
-  `token` varchar(255) NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -81,11 +81,11 @@ CREATE TABLE `password_reset_tokens` (
 
 CREATE TABLE `personal_access_tokens` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `tokenable_id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `token` varchar(64) NOT NULL,
-  `abilities` text DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `last_used_at` timestamp NULL DEFAULT NULL,
   `expires_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -100,9 +100,9 @@ CREATE TABLE `personal_access_tokens` (
 
 CREATE TABLE `pokemones` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `tipo` varchar(255) NOT NULL,
-  `tamaño` enum('grande','mediano','pequeño') NOT NULL,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tipo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tamaño` enum('grande','mediano','pequeño') COLLATE utf8mb4_unicode_ci NOT NULL,
   `peso` decimal(8,2) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -113,16 +113,26 @@ CREATE TABLE `pokemones` (
 --
 
 INSERT INTO `pokemones` (`id`, `nombre`, `tipo`, `tamaño`, `peso`, `created_at`, `updated_at`) VALUES
-(3, 'Bulbasur', 'planta', 'pequeño', 7.00, '2023-12-02 11:19:40', '2023-12-02 11:19:40'),
-(4, 'Vileplume', 'planta', 'mediano', 15.00, '2023-12-02 11:20:20', '2023-12-02 11:20:20'),
-(5, 'Squirtle', 'agua', 'pequeño', 9.00, '2023-12-02 11:20:41', '2023-12-02 11:20:41'),
-(6, 'Vaporeon', 'agua', 'mediano', 15.00, '2023-12-02 11:20:55', '2023-12-02 11:20:55'),
-(7, 'Charmander', 'fuego', 'pequeño', 8.00, '2023-12-02 11:21:13', '2023-12-02 11:21:13'),
-(8, 'Vulpix', 'fuego', 'pequeño', 6.00, '2023-12-02 11:21:30', '2023-12-02 11:21:30'),
-(9, 'Pikachu', 'electrico', 'pequeño', 6.00, '2023-12-02 11:21:45', '2023-12-02 11:21:45'),
-(10, 'Voltorb', 'electrico', 'mediano', 20.00, '2023-12-02 11:22:24', '2023-12-02 11:22:24'),
-(11, 'Rhyhorn', 'tierra', 'grande', 10.00, '2023-12-02 11:22:59', '2023-12-02 11:23:18'),
-(12, 'Cubone', 'tierra', 'pequeño', 8.00, '2023-12-02 11:23:57', '2023-12-02 11:23:57');
+(3, 'Bulbasur', 'planta', 'pequeño', '7.00', '2023-12-02 11:19:40', '2023-12-02 11:19:40'),
+(4, 'Vileplume', 'planta', 'mediano', '15.00', '2023-12-02 11:20:20', '2023-12-02 11:20:20'),
+(5, 'Squirtle', 'agua', 'pequeño', '9.00', '2023-12-02 11:20:41', '2023-12-02 11:20:41'),
+(6, 'Vaporeon', 'agua', 'mediano', '15.00', '2023-12-02 11:20:55', '2023-12-02 11:20:55'),
+(7, 'Charmander', 'fuego', 'pequeño', '8.00', '2023-12-02 11:21:13', '2023-12-02 11:21:13'),
+(8, 'Vulpix', 'fuego', 'pequeño', '6.00', '2023-12-02 11:21:30', '2023-12-02 11:21:30'),
+(9, 'Pikachu', 'electrico', 'pequeño', '6.00', '2023-12-02 11:21:45', '2023-12-02 11:21:45'),
+(10, 'Voltorb', 'electrico', 'mediano', '20.00', '2023-12-02 11:22:24', '2023-12-02 11:22:24'),
+(11, 'Rhyhorn', 'tierra', 'grande', '10.00', '2023-12-02 11:22:59', '2023-12-02 11:23:18'),
+(12, 'Cubone', 'tierra', 'pequeño', '8.00', '2023-12-02 11:23:57', '2023-12-02 11:23:57'),
+(13, 'Io5AQBEfbw', 'agua', 'pequeño', '85.00', NULL, NULL),
+(14, 'Wbpg5n3Rgl', 'agua', 'mediano', '24.60', NULL, NULL),
+(15, 'b2sGPOJ880', 'planta', 'grande', '74.40', NULL, NULL),
+(16, 'Tau0FqSI3k', 'agua', 'grande', '4.10', NULL, NULL),
+(17, 'CMmfZhrzae', 'electrico', 'grande', '97.70', NULL, NULL),
+(18, 'YS06wPcYSN', 'fuego', 'mediano', '76.00', NULL, NULL),
+(19, 'nS9XeoAkQb', 'planta', 'pequeño', '80.50', NULL, NULL),
+(20, 'ejK03qiFxW', 'planta', 'mediano', '24.10', NULL, NULL),
+(21, 'jRtIl62xWZ', 'tierra', 'mediano', '65.30', NULL, NULL),
+(22, 'E3zWpmhrjl', 'electrico', 'pequeño', '3.10', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -132,11 +142,11 @@ INSERT INTO `pokemones` (`id`, `nombre`, `tipo`, `tamaño`, `peso`, `created_at`
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) NOT NULL,
-  `remember_token` varchar(100) DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -211,7 +221,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT de la tabla `pokemones`
 --
 ALTER TABLE `pokemones`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
